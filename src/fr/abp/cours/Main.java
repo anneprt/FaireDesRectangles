39b5b46c6c7f4cfc6c7133a9c2ac100d2cd5d6b1
Anne <patesdegeek@gmail.com>
2020-04-26 10:47:24 +0200

    Initial commit

---
+++ src/fr/abp/cours/Main.java
@@ -0,0 +1,13 @@
+package fr.abp.cours;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Rectangle R= new Rectangle();
+        R.afficher();
+        R.créer();
+        R.afficher();
+        R.deplacer(0, 200);
+        R.afficher();
+    }
+}
